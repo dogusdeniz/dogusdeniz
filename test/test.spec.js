3e5e725e8a6cd635cb88ae7fc0ac5f8323fd126f
dogusdeniz <32245914+dogusdeniz@users.noreply.github.com>
2023-06-20 03:40:34 +0300

    fix : test bugfix

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,4 @@
-const { add } = require("../app/calculator.js");
+const { add, subtract } = require("../app/calculator.js");
 
 describe("add function", () => {
   it("should return the sum of two numbers", () => {
