46a531514f1d6ee558af5f91abe266bfe8bd768b
dogusdeniz <32245914+dogusdeniz@users.noreply.github.com>
2023-06-20 03:37:53 +0300

    update : codecov integration

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,16 +1,18 @@
+const { add } = require("./calculator");
+
 describe("add function", () => {
   it("should return the sum of two numbers", () => {
-    const result = 2 + 3;
+    const result = add(2, 3);
     expect(result).toBe(5);
   });
 
   it("should return seven", () => {
-    const result = 0 + 7;
+    const result = subtract(0, 7);
     expect(result).toBe(7);
   });
 
   it("should return a negative number when one of the numbers is negative", () => {
-    const result = -5 + 3;
+    const result = add(-5, 3);
     expect(result).toBe(-2);
   });
 });
